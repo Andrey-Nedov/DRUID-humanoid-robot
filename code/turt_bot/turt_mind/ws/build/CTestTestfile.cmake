0388189ba1bddc1dc9f5508153b886befa07f189
andreybelkovich <andrey.belkovich@users.noreply.github.com>
2021-07-02 21:17:23 +0300

    work

--- code/turt_bot/turt_mind/ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/andrey/Desktop/ros/sam/turt_mind/ws/src
-# Build directory: /home/andrey/Desktop/ros/sam/turt_mind/ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("control")
--- code/turt_bot/turt_mind/ws/build/control/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/andrey/Desktop/ros/sam/turt_mind/ws/src/control
-# Build directory: /home/andrey/Desktop/ros/sam/turt_mind/ws/build/control
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
